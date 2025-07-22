5f97f55b256f3a3541564b63865727f57854adb1
Wesley Maciel <weslmaciel@gmail.com>
2025-07-22 11:10:42 -0300

    rename domain

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: '/frontend-ascan/',
+	base: '/todo-fabrica/',
 	test: {
 		environment: 'jsdom',
 		globals: true,
